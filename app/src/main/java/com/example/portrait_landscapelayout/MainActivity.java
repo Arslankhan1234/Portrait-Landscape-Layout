a205876128fcfea59c19516a95a9259a42ccc7b2
Arslan Khan <bsef19a018@pucit.edu.pk>
2022-04-29 17:47:53 -0700

    Ome pic 3 buttons in portrait layout

---
+++ app/src/main/java/com/example/portrait_landscapelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.portrait_landscapelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
